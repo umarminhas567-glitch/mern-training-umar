3ae470ebfd7227982d089e6b1801c486f96bef68
umar <umarminhas567@gmail.com>
2026-09-15 08:39:51 +0500

    resolve 2 issues port and extra space

--- api/db.js
+++ api/db.js
@@ -5,6 +5,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'admin123',
   database: process.env.DB_NAME || 'mern_training',
+  port: process.env.DB_PORT || 3306,
 });
 
 module.exports = pool;
